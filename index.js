949a72e3094547d426188298ee09c1ff5e8050c4
Thales Araujo <thalesaraujobs@gmail.com>
2022-10-09 16:56:03 -0300

    Integreção front-back

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 
 dotenv.config()
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 let empregados = []
 
 app.get('/', (request, response) => {
@@ -15,7 +18,7 @@ app.post('/', (request,response) => {
     const id = (1 + empregados[empregados.length -1]?.id) || 1
     const novoFuncionario = {id, nome, cargo, salario}
     empregados.push(novoFuncionario)
-    response.json({message: 'Funcionário Criado!'})
+    response.json({message: 'Funcionario cadastrado!'})
 })
 
 app.get('/:id',(request, response) => {
